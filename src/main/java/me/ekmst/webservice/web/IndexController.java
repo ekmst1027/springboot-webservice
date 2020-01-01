495e4c68d3a8e3d8546c8959f3b9321ec5b66ff3
kyeongmin <ekmst1027@gmail.com>
2020-01-01 12:33:36 +0900

    5장 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기

--- src/main/java/me/ekmst/webservice/web/IndexController.java
+++ src/main/java/me/ekmst/webservice/web/IndexController.java
@@ -1,6 +1,8 @@
 package me.ekmst.webservice.web;
 
 import lombok.RequiredArgsConstructor;
+import me.ekmst.webservice.config.auth.LoginUser;
+import me.ekmst.webservice.config.auth.dto.SessionUser;
 import me.ekmst.webservice.service.posts.PostsService;
 import me.ekmst.webservice.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -15,8 +17,15 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model,
+                        @LoginUser SessionUser user) {
+
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
